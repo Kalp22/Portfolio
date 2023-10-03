ffb5868687c9a43bab41d6bf1c5ed0935e34a6b8
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-10-03 19:30:58 +0530

    Footer Added

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,9 +1,10 @@
 import styles from '../styles/footer.module.css'
 
-import {TbBrandNextjs} from 'react-icons/tb'
-import {BsLinkedin} from 'react-icons/bs'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { FaLinkedin } from 'react-icons/fa'
 import { FaSquareGithub } from "react-icons/fa6"
 import { FaSquareInstagram } from "react-icons/fa6"
+import { GrMail } from "react-icons/gr"
 
 
 export default function Footer() {
@@ -12,28 +13,34 @@ export default function Footer() {
         <div className={styles.footerParent}>
             <div className={styles.createBy}>
                 <span>Created By</span>
-                <span>Kalpesh</span>
+                <span className={styles.myName}>Kalpesh</span>
             </div>
             <div className={styles.createUsing}>
                 <span>Created using</span>
-                <div>
+                <a href='https://nextjs.org/' target='_blank'>
                     <TbBrandNextjs size={'45px'} />
-                </div>
+                </a>
             </div>
             <div className={styles.socialParent}>
                 <span>Social Media</span>
                 <div className={styles.socialSites}>
-                    <div className={styles.siteIcons}>
+
+                    <a href='mailto:kalpesh22nimje@gmail.com' target='_blank' className={styles.siteIcons}>
+                        <GrMail size={'35px'} />
+                    </a>
+
+                    <a href='https://github.com/Kalp22' target='_blank' className={styles.siteIcons}>
                         <FaSquareGithub size={'35px'} />
-                    </div>
-                    <div className={styles.siteIcons}>
-                        <BsLinkedin size={'35px'} />
+                    </a>
 
-                    </div>
-                    <div className={styles.siteIcons}>
+                    <a href='https://www.linkedin.com/in/kalpesh-nimje-305862234/' target='_blank' className={styles.siteIcons}>
+                        <FaLinkedin size={'35px'} />
+                    </a>
+
+                    <a href='https://instagram.com/kalpeshn003?igshid=OGQ5ZDc2ODk2ZA==' target='_blank' className={styles.siteIcons}>
                         <FaSquareInstagram size={'35px'} />
+                    </a>
 
-                    </div>
                 </div>
             </div>
         </div>
