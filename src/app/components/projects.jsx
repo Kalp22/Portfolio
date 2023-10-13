0a34b4791ad6582ff6ab12f06059e2b987271d88
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-10-14 00:27:28 +0530

    Projects added

--- src/app/components/projects.jsx
+++ src/app/components/projects.jsx
@@ -1,33 +1,48 @@
-// import styles from '../styles/projects.module.css'
+import styles from '../styles/projects.module.css'
 
-// export default function Projects() {
+export default function Projects() {
 
-//     return (
-//         <div className={styles.projectParent}>
-//             <div
-//                 className={["unselectable", "title"].join(" ")}
-//             >
-//                 Projects
-//             </div>
-//             <div className={styles.projectsCover}>
-//                 <div className={styles.theProjects}>
-//                     <span>Crunchyroll Copy</span>
-//                     <span>
-//                         enkhfdf
-//                     </span>
-//                     <div>
-//                         <a href='https://github.com/Kalp22/Crounchyroll_Copy' target='_blank' className={styles.sourceLink}>
-//                             Source Code
-//                         </a>
-//                         <a href='https://kalp22.github.io/Crounchyroll_Copy/' target='_blank' className={styles.viewLink}>
-//                             View Project
-//                         </a>
-//                     </div>
-//                 </div>
-//                 <div className={styles.theProjects}>
-
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
+    return (
+        <div className={styles.projectParent}>
+            <div
+                className={["unselectable", "title"].join(" ")}
+            >
+                Projects
+            </div>
+            <div className={styles.projectsCover}>
+                <div className={styles.theProjects}>
+                    <div className={styles.projectHeads}>
+                        <span className={styles.projectTitle}>Justice Index</span>
+                        <div>
+                            <a href='https://github.com/Mohitraut07/JusticeIndex' target='_blank' className={styles.sourceLink}>
+                                Source Code
+                            </a>
+                            <a href='https://justice-index-j2l9.vercel.app/' target='_blank' className={styles.viewLink}>
+                                View Project
+                            </a>
+                        </div>
+                    </div>
+                    <span className={styles.projectDescription}>
+                        A criminal database management system
+                    </span>
+                </div>
+                <div className={styles.theProjects}>
+                    <div className={styles.projectHeads}>
+                        <span className={styles.projectTitle}>Crunchyroll Copy</span>
+                        <div>
+                            <a href='https://github.com/Kalp22/Crounchyroll_Copy' target='_blank' className={styles.sourceLink}>
+                                Source Code
+                            </a>
+                            <a href='https://kalp22.github.io/Crounchyroll_Copy/' target='_blank' className={styles.viewLink}>
+                                View Project
+                            </a>
+                        </div>
+                    </div>
+                    <span className={styles.projectDescription}>
+                        A static copy of a famous entertainment website
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Navbar from './components/navbar'
 import Intro from './components/intro'
 import About from './components/aboutSection'
 import Technologies from './components/techIKnow'
-// import Projects from './components/projects'
+import Projects from './components/projects'
 import Footer from './components/footer'
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
       <Intro />
       <About />
       <Technologies />
-      {/* <Projects /> */}
+      <Projects />
       <Footer />
     </main>
   )
