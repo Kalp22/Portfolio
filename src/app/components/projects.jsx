b241dcffb2c947b512a677cd73ca2ca0b931a2a5
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-10-14 22:41:05 +0530

    Projects responsive and some tweaks

--- src/app/components/projects.jsx
+++ src/app/components/projects.jsx
@@ -12,14 +12,23 @@ export default function Projects() {
             <div className={styles.projectsCover}>
                 <div className={styles.theProjects}>
                     <div className={styles.projectHeads}>
-                        <span className={styles.projectTitle}>Justice Index</span>
-                        <div>
-                            <a href='https://github.com/Mohitraut07/JusticeIndex' target='_blank' className={styles.sourceLink}>
-                                Source Code
-                            </a>
-                            <a href='https://justice-index-j2l9.vercel.app/' target='_blank' className={styles.viewLink}>
-                                View Project
-                            </a>
+                        <span className={styles.projectTitle}>
+                            Justice Index
+                        </span>
+                        <span className={styles.projectDescript}>
+                            A criminal database management system
+                        </span>
+                        <div className={styles.linksWrapper}>
+                            <div className={styles.pLink}>
+                                <a href='https://github.com/Mohitraut07/JusticeIndex' target='_blank' className={styles.sourceLink}>
+                                    Source Code
+                                </a>
+                            </div>
+                            <div className={styles.pLink}>
+                                <a href='https://justice-index-j2l9.vercel.app/' target='_blank' className={styles.viewLink}>
+                                    View Project
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <span className={styles.projectDescription}>
@@ -28,14 +37,22 @@ export default function Projects() {
                 </div>
                 <div className={styles.theProjects}>
                     <div className={styles.projectHeads}>
-                        <span className={styles.projectTitle}>Crunchyroll Copy</span>
-                        <div>
-                            <a href='https://github.com/Kalp22/Crounchyroll_Copy' target='_blank' className={styles.sourceLink}>
-                                Source Code
-                            </a>
-                            <a href='https://kalp22.github.io/Crounchyroll_Copy/' target='_blank' className={styles.viewLink}>
-                                View Project
-                            </a>
+                        <span className={styles.projectTitle}>
+                            Crunchyroll Copy
+                        </span>
+                        <span className={styles.projectDescript}>
+                            A static copy of a famous entertainment website                        </span>
+                        <div className={styles.linksWrapper}>
+                            <div className={styles.pLink}>
+                                <a href='https://github.com/Kalp22/Crounchyroll_Copy' target='_blank' className={styles.sourceLink}>
+                                    Source Code
+                                </a>
+                            </div>
+                            <div className={styles.pLink}>
+                                <a href='https://kalp22.github.io/Crounchyroll_Copy/' target='_blank' className={styles.viewLink}>
+                                    View Project
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <span className={styles.projectDescription}>
