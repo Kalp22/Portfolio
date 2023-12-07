53dabd2ebfbaf4384e6d45b6498925939211fea8
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-12-07 21:58:52 +0530

    Update dependencies and project card styling

--- src/app/components/projectCard.jsx
+++ src/app/components/projectCard.jsx
@@ -6,7 +6,8 @@ export default function ProjectCard({ pData, key }) {
       <div className={styles.projectHeads}>
         <span className={styles.projectTitle}>{pData.name}</span>
         <span className={styles.projectDescript}>
-          A criminal database management system
+          {pData.description.substring(0, 100) +
+            (pData.description.length > 100 ? "..." : "")}
         </span>
         <div className={styles.linksWrapper}>
           <div className={styles.pLink}>
